de2f82f46d1e01b11f04a7956910ffb7fb10e9e0
An Na <99172832+An0401na@users.noreply.github.com>
2022-05-02 22:25:27 +0900

    .

---
+++ json_list_parsing/lib/JsonParseDemo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class JsonParseDemo extends StatefulWidget {
+  JsonParseDemo() : super();
+
+  @override
+  _JsonParseDemoState createState() => _JsonParseDemoState();
+}
+
+class _JsonParseDemoState extends State<JsonParseDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('Json Parsing'),
+    ));
+  }
+}
+
+//https://www.youtube.com/watch?v=wc8ecFSu5N8
